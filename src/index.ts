4bb162a4c94c560cb96ae0553d8e385a6c6ae401
Jay <jayvaidya3009@gmail.com>
2025-11-20 19:20:16 +0530

    Partial in typescript

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,15 @@ interface User {
   password: string;
 }
 
-type UpdateProps = Pick<User, "name" | "age" | "password">;
+type UpdateProps = Pick<User, "name" | "email" | "password">; // pick lets you pick certain values/keys from interface which reduce redandancy(duplication)
 
-function displayProfile(props: UpdateProps) {
-  console.log(`Name: ${props.name} and Age: ${props.age}`);
+type UpdatePropsOptional = Partial<UpdateProps>; // paritial(key?) is a utility which lets you make a key optional or here every key in UpdateProps is optional
+
+function updateUser(props: UpdatePropsOptional) {
+  //database call to update the user
 }
+
+updateUser({
+  email: "jayvaidya30@gmail.com", // here I can use one key of User interface because the other keys are set to partial i.e means I can user only one key if i want to
+});
+
