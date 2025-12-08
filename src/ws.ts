b490644c4de0aca35adb093aab0fdda3b3500785
Jay <jayvaidya3009@gmail.com>
2025-12-08 21:41:46 +0530

    basic boilerplate code and connection testing

--- src/ws.ts
+++ src/ws.ts
@@ -1,1 +1,14 @@
-console.log("hi");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//event handler
+wss.on("connection", function (socket) {
+  setInterval(() => {
+    socket.send("hi there"); //server is sending the message
+  }, 5000);
+
+  socket.on("message", (e) => {        //client is sending the message
+    console.log(e.toString);
+  });
+});
