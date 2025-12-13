d5c9f57849d0ce7940eab2845bd31b1f792f8afd
Jay <jayvaidya3009@gmail.com>
2025-12-14 01:19:20 +0530

    Using PostgresSQL as a database

--- src/ws.ts
+++ src/ws.ts
@@ -4,11 +4,12 @@ const wss = new WebSocketServer({ port: 8080 });
 
 //event handler
 wss.on("connection", function (socket) {
-  setInterval(() => {
-    socket.send("hi there"); //server is sending the message
-  }, 5000);
 
   socket.on("message", (e) => {        //client is sending the message
     console.log(e.toString);
+
+    if(e.toString() === "ping") {
+      socket.send("pong");
+    }
   });
 });
